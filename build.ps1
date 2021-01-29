bafe23b180cdd4072c84adbbc683c21618485ae5
Alex Manners <alexandermanners@gmail.com>
2021-01-29 13:37:15 -0500

    adding build powershell script for Windows users

---
+++ build.ps1
@@ -0,0 +1,6 @@
+param([String]$key="JWHQZM9Z4HQOYICDHW4OCJAXPPNHBA")
+go build -ldflags="-s -w -X main.key=$key" -o payloads/pneuma-windows.exe main.go;
+$env:GOOS='darwin';
+go build -ldflags="-s -w -X main.key=$key" -o payloads/pneuma-darwin main.go;
+$env:GOOS='linux';
+go build -ldflags="-s -w -X main.key=$key" -o payloads/pneuma-linux main.go;
