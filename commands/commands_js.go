1e1f6d930f57f109ff02749fa87968e225a8f46f
Alex Manners <alexandermanners@gmail.com>
2021-07-19 19:58:00 -0400

    Refactor executor to work with windows CMD properly

--- commands/commands_js.go
+++ commands/commands_js.go
@@ -1,7 +1,21 @@
 package commands
 
-import "syscall"
+import (
+	"context"
+	"os/exec"
+	"syscall"
+)
 
 func getSysProcAttrs() *syscall.SysProcAttr {
 	return &syscall.SysProcAttr{}
+}
+
+func getShellCommand(ctx context.Context, executor, command string) *exec.Cmd {
+	var cmd *exec.Cmd
+	switch executor {
+	default:
+		cmd = exec.CommandContext(ctx, "sh", "-c", command)
+	}
+	cmd.SysProcAttr = getSysProcAttrs()
+	return cmd
 }
--- commands/commands_other.go
+++ commands/commands_other.go
@@ -2,10 +2,30 @@
 
 package commands
 
-import "syscall"
+import (
+	"context"
+	"os/exec"
+	"syscall"
+)
 
 func getSysProcAttrs() *syscall.SysProcAttr {
 	return &syscall.SysProcAttr{
 		Setsid: true,
 	}
+}
+
+func getShellCommand(ctx context.Context, executor, command string) *exec.Cmd {
+	var cmd *exec.Cmd
+	switch executor {
+	case "python":
+		cmd = exec.CommandContext(ctx, "python", "-c", command)
+	case "osa":
+		cmd = exec.CommandContext(ctx, "osascript", "-e", command)
+	case "bash":
+		cmd = exec.CommandContext(ctx, "bash", "-c", command)
+	default:
+		cmd = exec.CommandContext(ctx, "sh", "-c", command)
+	}
+	cmd.SysProcAttr = getSysProcAttrs()
+	return cmd
 }
