0b3f1c9b05d39e1d773c8ddc4a5dcdd1b99a5a41
Alex Manners <alexandermanners@gmail.com>
2021-10-28 22:12:05 -0400

    Update readme.md

--- build.ps1
+++ build.ps1
@@ -1,7 +1,11 @@
 param([String]$randomHash="JWHQZM9Z4HQOYICDHW4OCJAXPPNHBA")
 $env:GOOS='windows';
-go build -ldflags="-s -w -X main.randomHash=$randomHash" -o payloads/pneuma-windows.exe main.go;
+go build -ldflags="-s -w -buildid= -X main.randomHash=$randomHash" -o payloads/pneuma-windows.exe main.go;
 $env:GOOS='darwin';
-go build -ldflags="-s -w -X main.randomHash=$randomHash" -o payloads/pneuma-darwin main.go;
+go build -ldflags="-s -w -buildid= -X main.randomHash=$randomHash" -o payloads/pneuma-darwin main.go;
 $env:GOOS='linux';
-go build -ldflags="-s -w -X main.randomHash=$randomHash" -o payloads/pneuma-linux main.go;
+go build -ldflags="-s -w -buildid= -X main.randomHash=$randomHash" -o payloads/pneuma-linux main.go;
+
+$env:GOOS='windows';
+$env:CGO_ENABLED=1;
+go build --buildmode=c-shared --ldflags="-s -w -buildid= -X main.randomHash=${1}" -o payloads/pneuma-windows.dll library/library.go;
